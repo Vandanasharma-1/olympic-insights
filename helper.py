9f5edad57b96f98b7392bcb6a315c87bd0ae3781
Vandanasharma-1 <158481544+Vandanasharma-1@users.noreply.github.com>
2024-05-17 19:17:13 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -77,13 +77,28 @@ def yearwise_medal_tally(df,country):
 
     return final_df
 
-def country_event_heatmap(df,country):
+#def country_event_heatmap(df,country):
+ #   temp_df = df.dropna(subset=['Medal'])
+  #  temp_df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'], inplace=True)
+
+   # new_df = temp_df[temp_df['region'] == country]
+
+    #pt = new_df.pivot_table(index='Sport', columns='Year', values='Medal', aggfunc='count').fillna(0)
+    #return pt
+
+def country_event_heatmap(df, country):
     temp_df = df.dropna(subset=['Medal'])
     temp_df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'], inplace=True)
 
     new_df = temp_df[temp_df['region'] == country]
 
+    if new_df.empty:
+        return None
+
     pt = new_df.pivot_table(index='Sport', columns='Year', values='Medal', aggfunc='count').fillna(0)
+    if pt.empty or pt.isnull().all().all():
+        return None
+
     return pt
 
 
